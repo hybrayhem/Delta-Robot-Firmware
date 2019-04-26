a0ebe14576111b9906e2d3da2323d858a16b8314
H. İbrahim İLHAN <47319650+hybrayhem38@users.noreply.github.com>
2019-04-26 10:01:32 +0300

    Update Triple_knob(artan).ino

--- DELTA ROBOT Firmware/_Triple_Knob/Triple_knob(artan).ino
+++ DELTA ROBOT Firmware/_Triple_Knob/Triple_knob(artan).ino
@@ -24,12 +24,14 @@ int potpin1 = 1;
 
 int potpin2 = 2;
 
+int dc = 5;
+
 int val;    // variable to read the value from the analog pin
-
+int newVal=90;
 int val1;
-
+int newVal1=90;
 int val2;
-
+//int newVal2=90;
 void setup() {
 
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
@@ -45,23 +47,26 @@ void loop() {
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
 
   val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-
-  myservo.write(val);                  // sets the servo position according to the scaled value
-
-  delay(15);                           // waits for the servo to get there
-
+  
+  newVal += Int(val/dc);
   
 
   val1 = analogRead(potpin1);            // reads the value of the potentiometer (value between 0 and 1023)
 
   val1 = map(val1, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-
+  newVal1 += Int(val1/dc);
+  if(newVal1=>0&&newVal1=<180&&newVal=>0&&newVal=<180){
+    val1 = newVal1;
+    val = newVal;
+   }
+ 
   myservo1.write(val1);                  // sets the servo position according to the scaled value
-
+  delay(15);                           // waits for the servo to get there
+  myservo.write(val);                  // sets the servo position according to the scaled value
   delay(15);                           // waits for the servo to get there
 
-  
-
+ 
+ 
   val2 = analogRead(potpin2);            // reads the value of the potentiometer (value between 0 and 1023)
 
   val2 = map(val2, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
